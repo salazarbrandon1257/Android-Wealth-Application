7e913375e7a2f85e95d01ddf2470473fb4a0c27f
salazarbrandon1257 <salazarbrandon1257@gmail.com>
2020-10-09 10:58:39 -0500

    Fixed chart percentages

--- app/src/main/java/com/example/divcal/ChartsActivity.java
+++ app/src/main/java/com/example/divcal/ChartsActivity.java
@@ -41,8 +41,8 @@ public class ChartsActivity extends AppCompatActivity {
         pieChart = findViewById(R.id.piechart);
         stockName1 = findViewById(R.id.stockNameOneId);
         stockName2 = findViewById(R.id.stockNameTwoId);
-        stockName3 = findViewById(R.id.stockNameThreeID);
-        stockName4 = findViewById(R.id.stockNameFourID);
+        stockName3 = findViewById(R.id.stockNameThreeId);
+        stockName4 = findViewById(R.id.stockNameFourId);
 
         final TextView portfolioValue  = (TextView) findViewById(R.id.portfolioValueId);
 
@@ -63,46 +63,59 @@ public class ChartsActivity extends AppCompatActivity {
         //}
         if(list.size() > 0) {
             //Toast.makeText(getApplicationContext(), list.get(0).toString(), Toast.LENGTH_SHORT).show();
-            Toast.makeText(getApplicationContext(), stockMap.get(list.get(list.size() - 1)), Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), Double.toString(sum), Toast.LENGTH_SHORT).show();
         }
         // Creating a method setData()
         // to set the text in text view and pie chart
-        setData(list, stockMap);
+        setData(list, stockMap, sum);
     }
 
-    private void setData(ArrayList<Double> list, HashMap<Double, String> stockMap) {
+    private void setData(ArrayList<Double> list, HashMap<Double, String> stockMap, double sum) {
         // Set the percentage of language used
-        tvR.setText(Integer.toString(40));
-        tvPython.setText(Integer.toString(30));
-        tvCPP.setText(Integer.toString(5));
-        tvJava.setText(Integer.toString(25));
-        if(stockMap.size() > 0){stockName1.setText(stockMap.get(list.get(list.size() - 1)));}
-        if(stockMap.size() > 1){stockName2.setText(stockMap.get(list.get(list.size() - 2)));}
-        if(stockMap.size() > 2){stockName3.setText(stockMap.get(list.get(list.size() - 3)));}
-        if(stockMap.size() > 3){stockName4.setText(stockMap.get(list.get(list.size() - 4)));}
+        Double valueOne;
+        Double valueTwo;
+        Double valueThree;
+        Double valueFour;
 
-        // Set the data and color to the pie chart
-        pieChart.addPieSlice(
-                new PieModel("lk",
-                        Integer.parseInt(tvR.getText().toString()),
-                        Color.parseColor("#FFA726")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Pythonkl",
-                        Integer.parseInt(tvPython.getText().toString()),
-                        Color.parseColor("#66BB6A")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "C++",
-                        Integer.parseInt(tvCPP.getText().toString()),
-                        Color.parseColor("#EF5350")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Java",
-                        Integer.parseInt(tvJava.getText().toString()),
-                        Color.parseColor("#29B6F6")));
-
-        // To animate the pie chart
+        if(stockMap.size() > 0){
+            valueOne =  new Double(list.get(list.size() - 1) * 100 / sum );
+            tvR.setText( Integer.toString(valueOne.intValue()) + "%");
+            stockName1.setText(stockMap.get(list.get(list.size() - 1)));
+            pieChart.addPieSlice(
+                    new PieModel("lk",
+                            Integer.parseInt(tvR.getText().toString().substring(0, tvR.getText().toString().length() - 1)),
+                            Color.parseColor("#FFA726")));
+        }
+        if(stockMap.size() > 1){
+            valueTwo =  new Double(list.get(list.size() - 2) * 100 / sum );
+            tvPython.setText(valueTwo.intValue() + "%");
+            stockName2.setText(stockMap.get(list.get(list.size() - 2)));
+            pieChart.addPieSlice(
+                    new PieModel(
+                            "Pythonkl",
+                            Integer.parseInt(tvPython.getText().toString().substring(0, tvPython.getText().toString().length() - 1)),
+                            Color.parseColor("#66BB6A")));
+        }
+        if(stockMap.size() > 2){
+            valueThree =  new Double(list.get(list.size() - 3) * 100 / sum );
+            tvCPP.setText(valueThree.intValue() + "%");
+            stockName3.setText(stockMap.get(list.get(list.size() - 3)));
+            pieChart.addPieSlice(
+                    new PieModel(
+                            "C++",
+                            Integer.parseInt(tvCPP.getText().toString().substring(0, tvCPP.getText().toString().length() - 1)),
+                            Color.parseColor("#EF5350")));
+        }
+        if(stockMap.size() > 3){
+            valueFour =  new Double(list.get(list.size() - 4) * 100 / sum );
+            tvJava.setText(valueFour.intValue() + "%");
+            stockName4.setText(stockMap.get(list.get(list.size() - 4)));
+            pieChart.addPieSlice(
+                    new PieModel(
+                            "Java",
+                            Integer.parseInt(tvJava.getText().toString().substring(0, tvJava.getText().toString().length() - 1)),
+                            Color.parseColor("#29B6F6")));
+        }
         pieChart.startAnimation();
     }
 }
